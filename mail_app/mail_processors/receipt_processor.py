ac00f2e9907bdd7d6c7def3447ef227887cc35f9
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2019-01-26 21:15:31 +0000

    feat(processors): add receipts processor

---
+++ mail_app/mail_processors/receipt_processor.py
@@ -0,0 +1,26 @@
+import re
+
+from mail_app.mail import Mail
+from mail_app.mail_processors.abstract_processor import AbstractProcessor
+from mail_app.processed_mail import ProcessedMail
+
+
+class ReceiptProcessor(AbstractProcessor):
+    general_keywords = ["receipt", "transfer sent"]
+
+    def __init__(self):
+        super().__init__()
+        self.category = "Receipts"
+
+    def process(self, mail):
+        if self.__general_conditions(mail):
+            return ProcessedMail(self.category, mail.body, mail.time, mail.attachments)
+
+    ############################################ Conditions ############################################
+
+    def __general_conditions(self, mail: Mail):
+        return (mail.attachments or
+                any(re.search(keyword, mail.subject.lower()) for keyword in self.general_keywords) or
+                any(re.search(keyword, mail.body.lower()) for keyword in self.general_keywords) or
+                any(re.search(keyword, attachment["name"].lower()) for attachment in mail.attachments for keyword in
+                    self.general_keywords))
