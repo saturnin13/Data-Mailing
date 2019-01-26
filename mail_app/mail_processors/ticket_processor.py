d842471377c5a9022ef5dd40118a0e11258ad75b
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2019-01-26 20:11:49 +0000

    feat(process): implement script exploitation

--- mail_app/mail_processors/ticket_processor.py
+++ mail_app/mail_processors/ticket_processor.py
@@ -9,7 +9,6 @@ from mail_app.processed_mail import ProcessedMail
 
 
 class TicketProcessor(AbstractProcessor):
-
     general_keywords = ["ticket", "boarding pass", "boardingpass", "booking reference"]
     ticket_keyword = ["qrcode"]
 
@@ -18,20 +17,17 @@ class TicketProcessor(AbstractProcessor):
         self.category = "Ticket"
 
     def process(self, mail: Mail) -> ProcessedMail:
-        if "Your Tickets for" in mail.subject:
-            self.__process_eventbrite(mail)
-        elif self.__general_conditions(mail):
-            return ProcessedMail(self.category, mail.body, mail.time, mail.attachments)
-
-    def __process_eventbrite(self, mail: Mail) -> ProcessedMail:
         soup = BeautifulSoup(mail.body, 'html.parser')
         schema_script = soup.find('script', type="application/ld+json")
-        schema_json = json.loads(schema_script.text)
-        processed_mail = ProcessedMail(category="event", description=schema_json['reservationFor']['name'], date=mail.time,
-                     attachments=mail.attachments, specifics=schema_json)
-        attrs = vars(processed_mail)
-        print(', '.join("%s: %s" % item for item in attrs.items()))
-        return processed_mail
+        if schema_script:
+            schema_json = json.loads(schema_script.text)
+            processed_mail = ProcessedMail(category=self.category, description=schema_json['reservationFor']['name'],
+                                           date=mail.time, attachments=mail.attachments, specifics=schema_json)
+            attrs = vars(processed_mail)
+            print(', '.join("%s: %s" % item for item in attrs.items()))
+            return processed_mail
+        if self.__general_conditions(mail):
+            return ProcessedMail(self.category, mail.body, mail.time, mail.attachments)
 
     ############################################ Conditions ############################################
 
