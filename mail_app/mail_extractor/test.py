951fc52fab09655e80a33bea69078f8d7c7c6555
Paul Vidal <paul.vidal14@imperial.ac.uk>
2019-01-27 01:47:24 +0000

    updated test for attachment extraction

--- mail_app/mail_extractor/test.py
+++ mail_app/mail_extractor/test.py
@@ -4,6 +4,7 @@ import base64
 import os.path
 import pickle
 import re
+from datetime import datetime
 
 import dateutil.parser as parser
 from bs4 import BeautifulSoup
@@ -41,90 +42,95 @@ def main():
 
     # start
 
-    unread_msgs = service.users().messages().list(userId='me').execute()
-
-    # We get a dictonary. Now reading values for the key 'messages'
-    mssg_list = unread_msgs['messages']
-
-    print("Total unread messages in inbox: ", str(len(mssg_list)))
-
-    final_list = []
-
-    for mssg in mssg_list:
-        temp_dict = {}
-        m_id = mssg['id']  # get id of individual message
-        message = service.users().messages().get(userId='me', id=m_id).execute()  # fetch the message using API
-        payld = message['payload']  # get payload of the message
-        headr = payld['headers']  # get header of the payload
-
-        for one in headr:  # getting the Subject
-            if one['name'] == 'Subject':
-                msg_subject = one['value']
-                temp_dict['Subject'] = msg_subject
-            else:
-                pass
-
-        for two in headr:  # getting the date
-            if two['name'] == 'Date':
-                msg_date = two['value']
-                date_parse = (parser.parse(msg_date))
-                m_date = (date_parse.date())
-                temp_dict['Date'] = str(m_date)
-            else:
-                pass
-
-        for three in headr:  # getting the Sender
-            if three['name'] == 'From':
-                msg_from = three['value']
-                temp_dict['Sender'] = msg_from
-            else:
-                pass
-
-        temp_dict['Snippet'] = message['snippet']  # fetching message snippet
-
-
-        # Fetching message body
-        mssg_parts = payld['parts']  # fetching the message parts
-        part_one = mssg_parts[0]  # fetching first element of the part
-
-        print(part_one['body'])
-
-        part_body = part_one['body']  # fetching body of the message
-        part_data = part_body['data']  # fetching data from the body
-        clean_one = part_data.replace("-", "+")  # decoding from Base64 to UTF-8
-        clean_one = clean_one.replace("_", "/")  # decoding from Base64 to UTF-8
-        clean_two = base64.b64decode(bytes(clean_one, 'UTF-8'))  # decoding from Base64 to UTF-8
-        soup = BeautifulSoup(clean_two, "lxml")
-        mssg_body = soup.body()
-        # mssg_body is a readible form of message body
-        # depending on the end user's requirements, it can be further cleaned
-        # using regex, beautiful soup, or any other method
-        temp_dict['Message_body'] = mssg_body
-
-        print(temp_dict)
+    # unread_msgs = service.users().messages().list(userId='me').execute()
+    #
+    # # We get a dictonary. Now reading values for the key 'messages'
+    # mssg_list = unread_msgs['messages']
+    #
+    # print("Total unread messages in inbox: ", str(len(mssg_list)))
+    #
+    # final_list = []
+    #
+    # for mssg in mssg_list:
+    #     temp_dict = {}
+    #     m_id = mssg['id']  # get id of individual message
+    #     message = service.users().messages().get(userId='me', id=m_id).execute()  # fetch the message using API
+    #     payld = message['payload']  # get payload of the message
+    #     headr = payld['headers']  # get header of the payload
+    #
+    #     for one in headr:  # getting the Subject
+    #         if one['name'] == 'Subject':
+    #             msg_subject = one['value']
+    #             temp_dict['Subject'] = msg_subject
+    #         else:
+    #             pass
+    #
+    #     for two in headr:  # getting the date
+    #         if two['name'] == 'Date':
+    #             msg_date = two['value']
+    #             date_parse = (parser.parse(msg_date))
+    #             m_date = (date_parse.date())
+    #             temp_dict['Date'] = str(m_date)
+    #         else:
+    #             pass
+    #
+    #     for three in headr:  # getting the Sender
+    #         if three['name'] == 'From':
+    #             msg_from = three['value']
+    #             temp_dict['Sender'] = msg_from
+    #         else:
+    #             pass
+    #
+    #     temp_dict['Snippet'] = message['snippet']  # fetching message snippet
+    #
+    #
+    #     # Fetching message body
+    #     mssg_parts = payld['parts']  # fetching the message parts
+    #     part_one = mssg_parts[0]  # fetching first element of the part
+    #
+    #     print(part_one['body'])
+    #
+    #     part_body = part_one['body']  # fetching body of the message
+    #     part_data = part_body['data']  # fetching data from the body
+    #     clean_one = part_data.replace("-", "+")  # decoding from Base64 to UTF-8
+    #     clean_one = clean_one.replace("_", "/")  # decoding from Base64 to UTF-8
+    #     clean_two = base64.b64decode(bytes(clean_one, 'UTF-8'))  # decoding from Base64 to UTF-8
+    #     soup = BeautifulSoup(clean_two, "lxml")
+    #     mssg_body = soup.body()
+    #     # mssg_body is a readible form of message body
+    #     # depending on the end user's requirements, it can be further cleaned
+    #     # using regex, beautiful soup, or any other method
+    #     temp_dict['Message_body'] = mssg_body
+    #
+    #     print(temp_dict)
 
     # Call the Gmail API
-    # results = service.users().messages().list(userId='me').execute()
+    results = service.users().messages().list(userId='me').execute()
 
-    # for m in results['messages'][0:3]:
-        # print(service.users().messages().get(userId='me', id=m['id']).execute()['payload'])
+    for m in results['messages'][2:3]:
+        print(service.users().messages().get(userId='me', id=m['id']).execute()['payload'])
 
-        # message = service.users().messages().get(userId='me', id=m['id'], format='raw').execute()
-        # msg_str = str(base64.urlsafe_b64decode(message['raw'].encode('UTF-8')))
-        #
-        # print(str(msg_str))
-        #
-        # time = datetime.fromtimestamp(int(message['internalDate']) / 1000)
-        # user_id = 'TODO'
-        # from_ = find_regex("smtp.mailfrom=(.*?)\\\\r", msg_str).replace(';', '')
-        # subject = find_regex("Subject: (.*?)\\\\r", msg_str)
-        # # body =
-        #
-        # print('Time: ' + str(time))
-        # print('from_: ' + from_)
-        # print('subject: ' + subject)
-        # print()
+        message = service.users().messages().get(userId='me', id=m['id'], format='raw').execute()
+        message2 = service.users().messages().get(userId='me', id=m['id'], format='full').execute()
+        attachment = service.users().messages().attachments().get(userId='me', messageId=m['id'], id='ANGjdJ94kr3hwDlYNg52eAjITiklR_Rz7wPsocoAHN3FWDUIHxpud1ak-S8IBfIzgImJSQASHM_Uj82g5BulidHGIJOY8oCBEoT1m7UOymU9OaZGjIQUoNggLm2EfG0Cvzc7igxCaZsb1JIpUhwXwXNft6llCfb-D2cC0Gazd8lb6wC3rMYRGpvwzwVjiElfFVW2l4Gt3zOkdwk6vQ57mvW1CPNluB5fYSlgbTiLplARrvet7iSW_k5igtwNY5_zXrE3mI_FXFamySeHOzJ0v6esp2fDxClTCNqZYCI25HENub0k_pSrQHawRV5ZbPfnO5dikfIcpyY9R2VZINqxzA0YoQPP8f2x5-_6OIhdfYaL8M9U7pfUqUMQmVxEgoPSlhPigHI6D5HZXbkJl_FV').execute()
 
+        print('Attachment')
+        print(attachment)
+        print()
+
+        msg_str = str(base64.urlsafe_b64decode(message['raw'].encode('UTF-8')))
+
+        time = datetime.fromtimestamp(int(message['internalDate']) / 1000)
+        user_id = 'TODO'
+        from_ = find_regex("smtp.mailfrom=(.*?)\\\\r", msg_str).replace(';', '')
+        subject = find_regex("Subject: (.*?)\\\\r", msg_str)
+        body = msg_str
+        attachments =
+
+        print('Time: ' + str(time))
+        print('from_: ' + from_)
+        print('subject: ' + subject)
+        print()
 
 
 def find_regex(regex, body):
